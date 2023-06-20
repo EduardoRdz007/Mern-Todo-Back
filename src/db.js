f7afe88a1e0540fe712ec9bc4c926e69ac4ecee8
EduardoRdz007 <edurdz010@gmail.com>
2023-06-19 18:53:37 -0600

    cambio de url

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async() =>{
     try {
-        await mongoose.connect('mongodb://localhost/merndb')
+        await mongoose.connect('mongodb://admin:oi8OOLj0rbQBURDRfI5VltfK@MongoS3601A.back4app.com:27017/1e13400d9a9c4b10b12865ebb4be6d86')
         console.log ("DB CONECTADA")
     } catch (error){
         console.log(error);
